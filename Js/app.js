f90657dc3ac8ff422e2ae5db2a5e6a825bbc70f1
radiation22 <radiationcorporation2@gmail.com>
2022-09-03 00:33:50 +0600

    show the card details

--- Js/app.js
+++ Js/app.js
@@ -1,3 +1,4 @@
+// loading category list
 const loadCategory = () => {
   const url = "https://openapi.programming-hero.com/api/news/categories";
   fetch(url)
@@ -8,11 +9,50 @@ const showNewsCategory = (categories) => {
   const categoryContainer = document.getElementById("category-container");
 
   categories.forEach((category) => {
-    console.log(category);
     const listDiv = document.createElement("li");
     listDiv.classList.add("nav-item");
-    listDiv.innerHTML = `<a class="nav-link active" aria-current="page">${category.category_name}</a>`;
+    listDiv.innerHTML = `
+    <a onclick="categoryNewsLoad('${category.category_id}')" class="nav-link active" aria-current="page">${category.category_name}</a>
+    `;
     categoryContainer.appendChild(listDiv);
   });
 };
+
+// showing categories news details for
+const categoryNewsLoad = (category_id) => {
+  const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showCategoryNews(data.data));
+};
+
+const showCategoryNews = (categoryNews) => {
+  console.log(categoryNews);
+  const card = document.getElementById("category-news");
+  card.innerHTML = "";
+  categoryNews.forEach((news) => {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("col");
+    console.log(news);
+    cardDiv.innerHTML = `
+    <div class="card mb-3" style="max-width: 540px;">
+    <div class="row g-0">
+        <div class="col-md-4">
+            <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+    </div>
+</div>
+    `;
+    card.appendChild(cardDiv);
+  });
+};
+
 loadCategory();
