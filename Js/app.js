eac2b39837c6c219e6e43bf95a0d858ff6065f96
radiation22 <radiationcorporation2@gmail.com>
2022-09-03 03:03:40 +0600

    modal added

--- Js/app.js
+++ Js/app.js
@@ -37,15 +37,21 @@ const showCategoryNews = (categoryNews) => {
     cardDiv.innerHTML = `
     <div class="card mb-3" style="max-width: 540px;">
     <div class="row g-0">
-        <div class="col-md-4">
-            <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+        <div class="col-md-5">
+            <img src="${news.thumbnail_url}" class="img-fluid h-100" alt="...">
         </div>
-        <div class="col-md-8">
+        <div class="col-md-7">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
-                    additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <h5 class="card-title">${news.title}</h5>
+                <p class="card-text">${news.details.slice(0, 120)}....</p>
+                <img src="${
+                  news.author.img
+                }" class="h-50 w-50 rounded-circle" alt="">
+                  <p>${news.author.name}</p>
+               
+                <button id="show-details" onclick="showCategoryDetails('${
+                  news._id
+                }')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Show News Details</button>
             </div>
         </div>
     </div>
@@ -53,6 +59,23 @@ const showCategoryNews = (categoryNews) => {
     `;
     card.appendChild(cardDiv);
   });
+};
+
+const showCategoryDetails = (news_id) => {
+  const url = `https://openapi.programming-hero.com/api/news/${news_id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => categoryDetails(data.data[0]));
+};
+
+const categoryDetails = (details) => {
+  console.log(details);
+  const title = document.getElementById("exampleModalLabel");
+  title.innerText = details.title;
+  const modal = document.getElementById("modal-img");
+  modal.innerHTML = `
+  <img src="${details.thumbnail_url}" alt="">
+  `;
 };
 
 loadCategory();
